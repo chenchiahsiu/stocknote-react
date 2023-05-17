5c1da4a75ba70d449dab127efd20f8c7ee9044e8
chenchiahsiu <hebe12311@gmail.com>
2023-05-17 16:00:38 +0800

    feat: adjust the position of posting modal

--- src/components/Modal/Posting/Posting.jsx
+++ src/components/Modal/Posting/Posting.jsx
@@ -17,72 +17,75 @@ const Posting = ({
 }) => {
   return (
     <div className={styles.PostingContainer}>
-      <div className={styles.TopSection}>
-        <div className={styles.TopSectionContainer}>
-          <div className={styles.Post}>
-            <Pen className={styles.Pen} />
-            <div>發文</div>
-          </div>
-          <div className={styles.Question}>
-            <Question className={styles.QuestionMark} />
-            <div>提問</div>
-          </div>
-          <button className={styles.Cancel} onClick={() => setPostModal('')}>
-            <div className={styles.CrossContainer}>
-              <Cross className={styles.Cross} />
+      <div className={styles.UpperContainer}>
+        <div className={styles.TopSection}>
+          <div className={styles.TopSectionContainer}>
+            <div className={styles.Post}>
+              <Pen className={styles.Pen} />
+              <div>發文</div>
             </div>
-          </button>
-        </div>
-      </div>
-      <div className={styles.CenterSection}>
-        <div className={styles.CenterSectionContainer}>
-          <div className={styles.User}>
-            <img src={Avatar} alt="" className={styles.Avatar} />
-            <span className={styles.Name}>Jasmine</span>
-          </div>
-          {inputValue === '' && (
-            <div className={styles.Text1}>
-              <span>想聊聊哪一支股票嗎？</span>
+            <div className={styles.Question}>
+              <Question className={styles.QuestionMark} />
+              <div>提問</div>
             </div>
-          )}
-          {inputValue === '' && (
-            <div className={styles.Button}>
-              <button>
-                <div>加權指數</div>
-              </button>
-              <button>
-                <div>鴻海</div>
-              </button>
-              <button>
-                <div>大樹</div>
-              </button>
-              <button>
-                <div>長榮</div>
-              </button>
-              <button className={styles.AnotherStock}>
-                <div className={styles.Magnifier}>
-                  <Magnifier />
-                </div>
-                <div className={styles.Text}>其它股票</div>
-              </button>
-            </div>
-          )}
-          <div className={inputValue === '' ? styles.Text2 : styles.Active}>
-            <textarea
-              placeholder="聊聊股市話題吧！"
-              value={inputValue}
-              onChange={(e) => {
-                onChange?.(e.target.value)
-              }}
-              onKeyDown={(e) => {
-                if (e.key === 'Enter') {
-                  onKeyDown?.()
-                }
-              }}
-            ></textarea>
+            <button className={styles.Cancel} onClick={() => setPostModal('')}>
+              <div className={styles.CrossContainer}>
+                <Cross className={styles.Cross} />
+              </div>
+            </button>
           </div>
         </div>
+        <div className={styles.CenterSection}>
+          <div className={styles.CenterSectionContainer}>
+            <div className={styles.User}>
+              <img src={Avatar} alt="" className={styles.Avatar} />
+              <span className={styles.Name}>Jasmine</span>
+            </div>
+            {inputValue === '' && (
+              <div className={styles.Text1}>
+                <span>想聊聊哪一支股票嗎？</span>
+              </div>
+            )}
+            {inputValue === '' && (
+              <div className={styles.Button}>
+                <button>
+                  <div>加權指數</div>
+                </button>
+                <button>
+                  <div>鴻海</div>
+                </button>
+                <button>
+                  <div>大樹</div>
+                </button>
+                <button>
+                  <div>長榮</div>
+                </button>
+                <button className={styles.AnotherStock}>
+                  <div className={styles.Magnifier}>
+                    <Magnifier />
+                  </div>
+                  <div className={styles.Text}>其它股票</div>
+                </button>
+              </div>
+            )}
+            <div className={inputValue === '' ? styles.Text2 : styles.Active}>
+              <textarea
+                placeholder="聊聊股市話題吧！"
+                value={inputValue}
+                onChange={(e) => {
+                  onChange?.(e.target.value)
+                }}
+                onKeyDown={(e) => {
+                  if (e.key === 'Enter') {
+                    onKeyDown?.()
+                  }
+                }}
+              ></textarea>
+            </div>
+          </div>
+        </div>
       </div>
+
       <div className={styles.BottomSection}>
         <div className={styles.BottomSectionContainer}>
           <div className={styles.Attachment}>
