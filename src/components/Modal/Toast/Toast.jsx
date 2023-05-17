91c0d39b02f5dca791cec35a986f8e5a5e2c746b
chenchiahsiu <hebe12311@gmail.com>
2023-05-17 08:52:02 +0800

    feat: add navigation on button

--- src/components/Modal/Toast/Toast.jsx
+++ src/components/Modal/Toast/Toast.jsx
@@ -4,7 +4,11 @@ import { ReactComponent as Cross } from 'assets/cross.svg'
 import { useContext } from 'react'
 import { LoginStateContext } from 'components/contexts/LoginStateContext'
 
+import { useNavigate } from 'react-router-dom'
+
 const Toast = () => {
+  const navigate = useNavigate()
+
   // 管理尚未登入的顯示 model
   const [loginState, setLoginState] = useContext(LoginStateContext)
 
@@ -19,7 +23,9 @@ const Toast = () => {
           <span>這個功能需要登入才可使用。</span>
           <div className={styles.Button}>
             <button onClick={() => setLoginState('')}>取消</button>
-            <button className={styles.Login}>登入</button>
+            <button className={styles.Login} onClick={() => navigate('/login')}>
+              登入
+            </button>
           </div>
         </div>
       </div>
